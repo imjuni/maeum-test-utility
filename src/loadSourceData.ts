259416ccecc632d7beb2cfa3eb4b0339157ce0de
ByungJoon Lee <imjuni@gmail.com>
2023-03-16 02:11:20 +0900

    Add dot notation in loadSourceData
    * add dot notation in loadSourceData
    * version change: 0.1.0 > 0.2.0

--- src/loadSourceData.ts
+++ src/loadSourceData.ts
@@ -1,6 +1,7 @@
+import { get } from 'dot-prop';
 import path from 'node:path';
 
-export default async function loadSourceData<T = unknown>(
+export default async function loadSourceData<T = Record<string, unknown>>(
   module: string,
   ...filePaths: string[]
 ): Promise<T> {
@@ -9,6 +10,6 @@ export default async function loadSourceData<T = unknown>(
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const imported = await import(filePath);
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  return imported[module] as T;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-argument
+  return get(imported, module) as T;
 }
